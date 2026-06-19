91d95ad90cc449667ccafe98d2e3a3ad6a571ccf
Nookie31247 <dudanmam@gmail.com>
2026-06-19 12:43:48 +0900

    Initial Commit (초기 세팅)

---
+++ dicom-back/src/main/java/com/allegro/dicomback/DicomBackApplication.java
@@ -0,0 +1,13 @@
+package com.allegro.dicomback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicomBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicomBackApplication.class, args);
+    }
+
+}
---
+++ dicom-back/src/test/java/com/allegro/dicomback/DicomBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allegro.dicomback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicomBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
